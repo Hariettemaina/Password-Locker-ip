8df93e41b0314040afacac28aff78e78fc718d5c
Hariettemaina <hariette.maina@student.moringaschool.com>
2022-04-24 20:03:25 +0300

    method to run before each test case

--- user_test.py
+++ user_test.py
@@ -1,2 +1,20 @@
 import unittest
 from user import User
+
+
+class Testuser(unittest.TestCase):
+    
+     '''
+    Test class that defines test cases for the User class behaviours.
+
+    Args:
+        unittest.TestCase: TestCase class that helps in creating test cases
+    '''
+    
+def setUp(self):
+    '''
+    Set up method to run before each test case 
+    '''
+    
+    #
+    
