578953ab289ee70edefc6bf6767fb41e5fe4afae
Hariettemaina <hariette.maina@student.moringaschool.com>
2022-04-24 21:43:12 +0300

    identation

--- user.py
+++ user.py
@@ -3,21 +3,21 @@
 
 class User:
     '''
-     class tha generates new instances of user account
+     class that generates new instances of user account
     '''
     #Empty list of users
     user_list = []
     
     
-    def __init__(self, user_name, user_password):
+    def __init__(self, username, password):
         '''
         __init__ method to define the properties of a User object
         Args:
             user_name : name of a user
             user_password : password for a user
         '''
-        self.user_name = user_name
-        self.user_password = user_password
+        self.user_name = username
+        self.user_password = password
         
         
     def delete_user(self):
--- user_test.py
+++ user_test.py
@@ -10,36 +10,36 @@ class TestUser(unittest.TestCase):
     Args:
         unittest.TestCase: TestCase class that helps in creating test cases
     '''
-    
+
 def setUp(self):
-    '''
-    Set up method to run before each test casees. 
-    '''
-    
-    #Create a user object
-    self.new_user = User('Jolly2022' '12345')
-    
-def test_init_(self):
-    '''
-    Test case to check if the object is initialized properly
-    '''
-    
-    self.asserEqual( self.new_user.user_name, 'Jolly')
-    self.assertEqual( self.new_user.user_password, '12345')
-    
+        '''
+        Set up method to run before each test cases. 
+        '''
+
+        #Create a user object
+        self.new_user = User('Jolly2022','12345')
+
+def test_init(self):
+        '''
+        Test case to check if the object is initialized properly
+        '''
+
+        self.asserEqual(self.new_user.username,'Jolly')
+        self.assertEqual(self.new_user.password,'12345')
+
 def test_save_user(self):
-    '''
-    test_save_user test case to test if the user object is saved to the user list
-    '''
-    self.new_user.save_user()
-    self.assertEqual(len(User.user_list),1)
-    
+        '''
+        test_save_user test case to test if the user object is saved to the user list
+        '''
+        self.new_user.save_user()
+        self.assertEqual(len(User.user_list),1)
+
 def test_delete_user(self):
-    '''
-    test_delete_user to test if you can remove user from user list 
-    '''
-    self.new_delete_user()
-    self.assertEqual(len(User.user_list),1)    
-    
+        '''
+        test_delete_user to test if you can remove user from user list 
+        '''
+        self.new_delete_user()
+        self.assertEqual(len(User.user_list),1)    
+
 if __name__ == '__main__':
     unittest.main()    
