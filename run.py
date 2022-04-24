6ee0343259b3a45c4a019599cb9ff01a54a0e48d
Hariettemaina <hariette.maina@student.moringaschool.com>
2022-04-25 01:06:07 +0300

    method for existinsg user name and save users

--- run.py
+++ run.py
@@ -18,3 +18,21 @@ def create_user(name, password):
      new_user = User(name,password)
 
      return new_user
+
+def save_users(user):
+    '''
+    Function to save a user account
+    Args:
+        user : the user account to be saved
+    '''
+
+    user.save_user()
+
+def check_existing_users(name):
+    '''
+    Function that checks if a user account name already exists
+    Args:
+        name : the user account name
+    '''
+
+    return User.user_exist(name)
