8bebae96db91730e3b8bac55a05fbdc8d8741864
Hariettemaina <hariette.maina@student.moringaschool.com>
2022-04-24 22:56:27 +0300

    fixed a bug

--- user.py
+++ user.py
@@ -9,22 +9,16 @@ class User:
     user_list = []
     
     
-    def __init__(self, username, password):
+    def __init__(self, user_name, password):
         '''
         __init__ method to define the properties of a User object
         Args:
             user_name : name of a user
             user_password : password for a user
         '''
-        self.user_name = username
+        self.user_name = user_name
         self.user_password = password
-        
-        
-    def delete_user(self):
-        '''
-        delete_account method deletes a  saved account from the list
-        '''
-        User.user_list.remove(self)   
+             
         
     def save_user(self):
         '''
@@ -32,4 +26,8 @@ class User:
         '''
         User.user_list.append(self)
     
-        
+    def delete_user(self):
+        '''
+        delete_account method deletes a  saved account from the list
+        '''
+        User.user_list.remove(self)     
--- user_test.py
+++ user_test.py
@@ -1,45 +1,47 @@
 import unittest
 from user import User
 
-
 class TestUser(unittest.TestCase):
     
-     '''
-    Test class that defines test cases for the User class behaviours.
-
-    Args:
-        unittest.TestCase: TestCase class that helps in creating test cases
-    '''
-
-def setUp(self):
         '''
-        Set up method to run before each test cases. 
+        Test class that defines test cases for the User class behaviours.
+
+        Args:
+            unittest.TestCase: TestCase class that helps in creating test cases
         '''
 
-        #Create a user object
-        self.new_user = User('Jolly2022','12345')
+        def setUp(self):
+            '''
+            Set up method to run before each test cases. 
+            '''
 
-def test_init(self):
-        '''
-        Test case to check if the object is initialized properly
-        '''
+            #Create a user object
+            self.new_user = User('Hariettemaina','12345')
 
-        self.asserEqual(self.new_user.username,'Jolly')
-        self.assertEqual(self.new_user.password,'12345')
+        def test_init(self):
+            '''
+            Test case to check if the object is initialized properly
+            '''
 
-def test_save_user(self):
-        '''
-        test_save_user test case to test if the user object is saved to the user list
-        '''
-        self.new_user.save_user()
-        self.assertEqual(len(User.user_list),1)
+            self.assertEqual(self.new_user.user_name, 'Hariettemaina')
+            self.assertEqual(self.new_user.user_password, '12345')
 
-def test_delete_user(self):
-        '''
-        test_delete_user to test if you can remove user from user list 
-        '''
-        self.new_delete_user()
-        self.assertEqual(len(User.user_list),1)    
+        def test_save_user(self):
+            '''
+            test_save_user test case to test if the user object is saved to the user list
+            '''
+            self.new_user.save_user()
+            self.assertEqual(len(User.user_list),1)
+
+        def test_delete_user(self):
+            '''
+            test_delete_user to test if you can remove user from user list 
+            '''
+            self.new_user.save_user()
+            test_user = User('Test','67890') #new user
+            test_user.save_user()
+            self.new_user.delete_user() #deleting user
+            self.assertEqual(len(User.user_list),1)    
 
 if __name__ == '__main__':
     unittest.main()    
